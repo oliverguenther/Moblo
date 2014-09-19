7376416cb020b4f29cc3b8a1ca6cfe4ee793e818
Oliver Günther <mail@oliverguenther.de>
2014-09-19 21:21:04 +0200

    Post 2: Routing and Rendering

--- lib/Moblo.pm
+++ lib/Moblo.pm
@@ -1,18 +1,14 @@
 package Moblo;
 use Mojo::Base 'Mojolicious';
 
-# This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+    # Router
+    my $r = $self->routes;
 
-  # Router
-  my $r = $self->routes;
-
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+    # GET / -> Main::index()
+    $r->get('/')->to(template => 'main/index');
 }
 
 1;
